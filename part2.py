b75d3941dbc43dd3182547cab24f7f8f5eb65ae7
asturtz2 <alexstur@gmail.com>
2018-02-27 22:08:13 -0600

    flatten input placeholder

--- part2.py
+++ part2.py
@@ -102,7 +102,7 @@ def main(argv):
     saver.restore(session, FLAGS.save_dir + '/emodb_homework_2-0')
     graph = session.graph
     print(graph.get_operations())
-    x = tf.reshape(graph.get_tensor_by_name('input_placeholder:0'), [-1, 129,129,1])
+    x = tf.reshape(graph.get_tensor_by_name('input_placeholder:0'), [None, 16641])
     y = tf.placeholder(tf.float32, [None, 7], name='label')
     hidden = graph.get_tensor_by_name('Conv_model/dense/Relu:0')
     dense_out = tf.stop_gradient(hidden)
